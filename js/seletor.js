68f990509acc81fbe467a1df9917fe142a5c3739
Andrierlison <58059077+Andrierlison@users.noreply.github.com>
2020-04-26 15:23:31 -0300

    Update seletor.js

--- js/seletor.js
+++ js/seletor.js
@@ -1,7 +1,7 @@
-function mos() {
+function mostrarCor() {
     var red = document.getElementById("red").value
     var green = document.getElementById("green").value
     var blue = document.getElementById("blue").value
-    document.getElementById("res").innerHTML = `<h2>RBG(${red}, ${green}, ${blue})</h2>`
-    document.getElementById("res").style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")"
+    document.getElementById("resultado").innerHTML = `<h2>RBG(${red}, ${green}, ${blue})</h2>`
+    document.getElementById("resultado").style.backgroundColor = "rgb(" + red + ", " + green + ", " + blue + ")"
   }
